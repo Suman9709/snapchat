45098367b1cf92668adda41fbc3b7996016b7f8d
Suman Kumar <suman.k9709@gmail.com>
2026-07-27 11:57:43 +0530

    updated admin .py page to handle the admin user for login  and added some function in th eutils

--- snapchat/admin.py
+++ snapchat/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
 from snapchat.models import Conversation, FriendRequest, Message, SnapUser
 
 # Register your models here.
+
+# if unable to use the user created by the admin
+class MyUserAdmin(UserAdmin):
+    model = SnapUser
+    fields = UserAdmin.fieldsets+((None, {"fields":("profile_pic","bio")}))
+    
+    
 admin.site.register(FriendRequest)
 admin.site.register(Conversation)
 admin.site.register(Message)
--- snapchat/utils.py
+++ snapchat/utils.py
@@ -7,3 +7,7 @@ def are_friends(user1, user2):
         Q(from_user=user2, to_user=user1),
         status=FriendRequest.StatusChoices.ACCEPTED,
     ).exists()
+
+
+def streak(user1, user2):
+    pass
